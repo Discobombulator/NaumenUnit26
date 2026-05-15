8e70692f4c7f55a3b0c0e4349532c6976c3fe12f
Ozornin Yaroslav <Runboll023@gmail.com>
2026-05-15 20:13:45 +0500

    Initial commit

---
+++ src/main/java/ru/Batareya/NaumenUnit26/NaumenUnit26Application.java
@@ -0,0 +1,13 @@
+package ru.Batareya.NaumenUnit26;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class NaumenUnit26Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(NaumenUnit26Application.class, args);
+	}
+
+}
---
+++ src/test/java/ru/Batareya/NaumenUnit26/NaumenUnit26ApplicationTests.java
@@ -0,0 +1,13 @@
+package ru.Batareya.NaumenUnit26;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class NaumenUnit26ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
